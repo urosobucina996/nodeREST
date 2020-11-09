5d6b38a1e38ceb553d1de82edda78d47fa298aa2
Serverside <obucina.uros.1@gmail.com>
2020-11-09 13:41:37 +0100

    Async and await

--- routes/workers.js
+++ routes/workers.js
@@ -12,10 +12,10 @@ route.use(bodyParser.urlencoded({ extended: true }));
 
 // --------  Functions for CRUD ----------- //
 
-function find(res,parameter = null,restrictions = null){
+ function find(res,parameter = null,restrictions = null){
 
     const param = parameter ? {'name' : parameter} : {};
-    const restr = restrictions ? {} : {'_id' : 0, '__v':0};
+    const restr = restrictions ? {} : { '__v':0};
 
     mongoose.find(param, restr)
         .then((data) => {
@@ -58,34 +58,34 @@ function updateRow(data, res){
 
 
 // -------- Routes --------- //
-route.get('/', (req, res) => {
+route.get('/', async (req, res) => {
 
     //Get data from collection
-    find(res);
+    await find(res);
 
 });
 
 
 // Pass parametar
-route.get('/:name', (req, res) => {
+route.get('/:name', async (req, res) => {
 
     //res.send(req.params.name);
     // Dinamicly make fields for show data.
     const restrictions = ['_id','name'];
-    find(res,req.params.name);
+    await find(res,req.params.name);
 
 });
 
-route.post('/', (req, res) => {
+route.post('/', async (req, res) => {
 
-    inserOne(req.body,res);
+    await inserOne(req.body,res);
     //res.redirect('/api/');
 });
 
-route.patch('/', (req, res) => {
+route.patch('/', async (req, res) => {
 
 
-    updateRow(req.body,res);
+    await updateRow(req.body,res);
 
 });
 
